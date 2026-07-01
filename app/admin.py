53336fdea99e3629c36e763775a37ac6784fef86
User <amirsaidovakmal7@gmail.com>
2026-07-01 12:27:25 +0500

    update

--- app/admin.py
+++ app/admin.py
@@ -3,11 +3,5 @@ from django.contrib import admin
 from .models import *
 
 
-
-
-
-
-
-
 admin.site.register(House)
-
+admin.site.register(House_photo)
--- app/models.py
+++ app/models.py
@@ -6,11 +6,6 @@ class House(models.Model):
     house_name = models.CharField(max_length=128, verbose_name='Название домика')
     house_description = models.TextField('Описание для домика')
     house_price = models.DecimalField(max_digits=25, decimal_places=2, verbose_name='Стоимость аренды в суммах')
-    house_photo1 = models.ImageField(upload_to='media', default='', verbose_name='Фотография домика', null=True, blank=True)
-    house_photo2 = models.ImageField(upload_to='media', default='', verbose_name='Фотография домика', null=True, blank=True)
-    house_photo3 = models.ImageField(upload_to='media', default='', verbose_name='Фотография домика', null=True, blank=True)
-    house_photo4 = models.ImageField(upload_to='media', default='', verbose_name='Фотография домика', null=True, blank=True)
-    house_photo5 = models.ImageField(upload_to='media', default='', verbose_name='Фотография домика', null=True, blank=True)
     class Meta:
         verbose_name = 'Домик'
         verbose_name_plural = 'Домики'
@@ -20,3 +15,12 @@ class House(models.Model):
 
 
 
+class House_photo(models.Model):
+    house_name = models.TextField('Название домика', default='Домик')
+    house_photo = models.ImageField(upload_to='media', verbose_name='Фотография домика')
+    house = models.ForeignKey(House, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = 'Фото'
+        verbose_name_plural = 'Фотки'
+    def __str__(self):
+        return self.house_name
