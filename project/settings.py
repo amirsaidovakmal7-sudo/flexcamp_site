937217b3fb307034d24aadcd1c882b91a830a2ac
User <amirsaidovakmal7@gmail.com>
2026-07-01 13:40:47 +0500

    update settings

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b+^tt8r*!3r%-as4mjgg^9phbheazu1fbw&c#&fr*(k#lib8mq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['flexcamp.uz', 'www.flexcamp.uz']
+
 
 
 # Application definition
